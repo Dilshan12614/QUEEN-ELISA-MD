eacc8311c8331884541314819e2c8e2b80bc5dc2
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-19 12:27:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "", // Koyeb/Render එකේ දාන code එක මෙතනට ඔටෝ ගන්නවා
+    SESSION_ID: process.env.SESSION_ID || "LUXALGO=P5xkUZYL#kmvWI2vvod2HmcT_qDBzNHq2GD44jeHfakgGubLJPwU", // Koyeb/Render එකේ දාන code එක මෙතනට ඔටෝ ගන්නවා
     BOT_NAME: "QUEEN ELISA-MD"
 };
